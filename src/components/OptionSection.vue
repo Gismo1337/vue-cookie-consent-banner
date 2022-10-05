2e7ff68361fd56e18438145ddebbd9e42e7f9569
Gismo1337 <richter@warespace.de>
2022-10-05 16:48:08 +0200

    checkbox styling

--- src/components/OptionSection.vue
+++ src/components/OptionSection.vue
@@ -7,57 +7,90 @@ const props = defineProps({
 <template>
   <section class="modal-options" id="options">
     <slot name="options">
-      <div class="check-group">
-        <Transition name="fade" mode="out-in">
+
+      <div class="options-container">
+        <div class="anal-div">  <Transition name="fade" mode="out-in">
           <input
-            type="checkbox"
-            id="analytics"
-            :key="cookiesStatus.analytics"
-            v-model="cookiesStatus.analytics"
+              type="checkbox"
+              id="analytics"
+              :key="cookiesStatus.analytics"
+              v-model="cookiesStatus.analytics"
           />
         </Transition>
-        <label for="analytics">Analytics</label>
-        <slot>
-          - Analytics cookies are used to track website visitors and their user behaviour.
-        </slot>
-      </div>
+          <label for="analytics">Analytics</label>
+          <p class="sub-text">
+            Analytics cookies are used to track website visitors and their user behaviour.
+          </p>
+        </div>
 
-      <div class="check-group">
-        <Transition name="fade" mode="out-in">
+        <div class="mark-div">  <Transition name="fade" mode="out-in">
           <input
-            type="checkbox"
-            id="marketing"
-            :key="cookiesStatus.marketing"
-            v-model="cookiesStatus.marketing"
+              type="checkbox"
+              id="marketing"
+              :key="cookiesStatus.marketing"
+              v-model="cookiesStatus.marketing"
           />
         </Transition>
-        <label for="marketing">Marketing</label>
-        <slot>
-          - Marketing Cookies are used to send advertising, or to track the user across several websites for marketing purposes.
-        </slot>
+          <label for="marketing">Marketing</label>
+          <p class="sub-text">
+            Marketing Cookies are used to send advertising, or to track the user across several websites for marketing purposes.
+          </p>
+        </div>
+
+        <div class="essent-div">   <!-- NO v-model because required! -->
+          <input checked disabled type="checkbox" id="essential" />
+          <label for="essential">Essential</label>
+          <p class="sub-text">
+            Essential cookies are essential for a website to function correctly.
+          </p></div>
       </div>
 
-      <div class="check-group">
-        <!-- NO v-model because required! -->
-        <input checked disabled type="checkbox" id="essential" />
-        <label for="essential">Essential</label>
-        <slot>
-          - Essential cookies are essential for a website to function correctly.
-        </slot>
-      </div>
+
     </slot>
   </section>
 </template>
 
 <style>
-.check-group {
-  padding: 10px; /* Some padding */
-  cursor: pointer; /* Pointer/hand icon */
-  float: left; /* Float the checkboxes side by side */
-  color: rgb(93, 92, 92);
+.options-container {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: 1fr;
+  grid-column-gap: 1em;
+  grid-row-gap: 0em;
+  margin: 0em 1em 1em 1em;
 }
 
-/* Here fade animation css */
+.anal-div { grid-area: 1 / 1 / 2 / 2; }
+.mark-div { grid-area: 1 / 2 / 2 / 3; }
+.essent-div { grid-area: 1 / 3 / 2 / 4; }
+
+@media only screen and (max-width: 400px) {
+  /* For mobile phones: */
+  .options-container {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(1, 1fr);
+    grid-column-gap: 0px;
+    grid-row-gap: 1em;
+    margin: 0em 1em 1em 1em;
+  }
+
+  .anal-div { grid-area: 1 / 1 / 2 / 2; }
+  .mark-div { grid-area: 2 / 1 / 3 / 2; }
+  .essent-div { grid-area: 3 / 1 / 4 / 2; }
+}
+
+
+label {
+  font-weight: bold;
+  margin-left: 1em;
+}
+
+.sub-text {
+  font-size: 0.8em;
+  color: #666;
+}
+
 .fade-enter-active,
 .fade-leave-active {
   transition: opacity 0.25s ease;
