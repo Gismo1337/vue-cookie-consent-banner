1f8492cd07cc7dd80204027806c86465a683685c
Gismo1337 <richter@warespace.de>
2022-10-09 14:07:36 +0200

    change cookie expire to 2099

--- src/components/CookieModal.vue
+++ src/components/CookieModal.vue
@@ -40,7 +40,7 @@ const accept = (value) => {
   // Save preferences to cookie
   document.cookie = `${COOKIE_NAME}=${JSON.stringify(
     cookiesStatus.value
-  )}; expires=Fri, 31 Dec 9999 23:59:59 GMT;`;
+  )}; expires=Fri, 31 Dec 2099 23:59:59 GMT;`;
 
   emit("accept", value);
   close();
