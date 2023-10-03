d1d82cc18b00756aaa3ddf22d5ba6cd2a8cbb10c
Gismo1337 <richter@warespace.de>
2023-10-03 18:59:39 +0200

    fix spell

--- src/components/CookieModal.vue
+++ src/components/CookieModal.vue
@@ -78,7 +78,7 @@ const toggleOptions = () => {
             class="btn-close-svg"
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 384 512">
-            <!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+            <!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Font icons, Inc. -->
             <path d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1
                      4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9
                      70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 
